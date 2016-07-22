8c44d7992f0891f902f19768e98710600fbbf780
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2016-07-22 11:29:43 +0100

    Add GitHub webhook
    - A post route has been added to Sinatra to handle a payload from GitHub after a webhook is triggered. The webhook is triggered when a pull request is opened, closed or edited in any way.  Sinatra then instantiates an inst§ance of the GitHubPullRequest class, passing in data from the payload.
    - Given that the payload deals with one pull request only, the GitHubPrScraper class was reworked and simplified to process just one pull request
    - GitHubPrScraper has been renamed GitHubPullRequest to reflect the fact that it is no longer scraping an organisation's pull requests
    - github_api_spec tests have now been deleted as pr_poster_spec is handling the API interactions, making the GitHub API specific tests redundant

--- spec/github_pr_scraper_spec.rb
+++ spec/github_pr_scraper_spec.rb
@@ -1,67 +1,31 @@
-require 'github_pr_scraper'
+require 'github_pull_request'
+require 'ostruct'
 
-describe GitHubPrScraper do
-  subject(:scraper) { GitHubPrScraper.new }
-
-  let(:octokit) { double Octokit::Client }
-  let(:trello_poster) { double TrelloPoster }
-  let(:commits) do
-    { 'https://github.com/alphagov/transition/pull/511' =>
-      'A commit with a Trello URL https://trello.com/c/Xf9vMxZ9/5-grab-all-commit-messages-from-open-pull-requests' }
+describe GitHubPullRequest do
+  let(:trello_poster) { double(TrelloPoster, new: nil)}
+  let(:repo_pull_request) do
+    OpenStruct.new({
+      html_url: 'https://github.com/gov-test-org/project-b/pull/1',
+      body: 'A commit with a Trello URL https://trello.com/c/6wQLN2C7/21-add-randomfile-1-txt-to-project-b'
+    })
   end
-  let(:repo_pull_requests) do
-    { items: [
-      { pull_request: {
-          html_url: 'https://github.com/alphagov/transition/pull/511' },
-        body: 'A commit with a Trello URL https://trello.com/c/Xf9vMxZ9/5-grab-all-commit-messages-from-open-pull-requests'
-      }
-    ] }
-  end
-  let(:prs_and_trello_card_ids) do
-    { 'https://github.com/alphagov/transition/pull/511' => 'Xf9vMxZ9'}
-  end
+  let(:octokit) { double Octokit::Client, login: 'emmabeynon', pull_request: repo_pull_request }
+  subject(:scraper) { GitHubPullRequest.new(60356369, 1, trello_poster) }
 
   before(:each) do
-    allow(octokit).to receive(:new).and_return(octokit)
-    allow(octokit).to receive(:auto_paginate=).and_return true
-    allow(scraper.login_user).to receive(:search_issues).and_return(repo_pull_requests)
+    allow(Octokit::Client).to receive(:new).and_return(octokit)
   end
 
   describe 'Default' do
     it 'initializes with login_user set to an authenticated Github user' do
       expect(scraper.login_user).to have_attributes(login: 'emmabeynon')
     end
-
-    it 'initializes with pull_requests set to nil' do
-      expect(scraper.pull_requests).to be_nil
-    end
-
-    it 'initializes with commits set to nil' do
-      expect(scraper.commits).to be_nil
-    end
   end
 
-  describe '#fetch_pull_requests' do
-    it 'returns a list of open pull requests from repos on Alphagov' do
-      scraper.fetch_pull_requests
-      expect(scraper.pull_requests).to eq repo_pull_requests[:items]
-    end
-  end
-
-  describe '#fetch_commits' do
-    it 'returns a list of commits from open pull requests on Alphagov' do
-      scraper.fetch_pull_requests
-      scraper.fetch_commits
-      expect(scraper.commits).to eq commits
-    end
-  end
-
-  describe '#filter_commits' do
-    it 'filters commits for those containing Trello card URLs and initializes a new trello poster instance' do
-      expect(TrelloPoster).to receive(:new).with(commits.keys.first, 'Xf9vMxZ9').and_return(trello_poster)
-      scraper.fetch_pull_requests
-      scraper.fetch_commits
-      scraper.filter_commits
+  describe '#fetch_pull_request_data' do
+    it 'retrieves HTML URL and body data for the given URL and passes to the check_for_trello_card method, leading to GitHubPullRequest being instantiated' do
+      expect(trello_poster).to receive(:new).with(repo_pull_request[:html_url], '6wQLN2C7')
+      GitHubPullRequest.new(60356369, 1, trello_poster)
     end
   end
 end
