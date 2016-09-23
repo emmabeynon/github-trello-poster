74de25a20f46319945d2bf565e67519c4fdfb6fe
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2016-09-23 11:59:02 +0100

    Add Sinatra logging
    Trello card: https://trello.com/c/x2bJMpI8/44-set-up-sinatra-logging
    The app now logs requests and errors.

--- app.rb
+++ app.rb
@@ -3,8 +3,23 @@ require 'dotenv'
 Dotenv.load
 require './lib/github_pull_request'
 require 'json'
+require 'logger'
 
 class GithubTrelloPoster < Sinatra::Base
+
+  ::Logger.class_eval { alias :write :'<<' }
+  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'.','log','access.log')
+  access_logger = ::Logger.new(access_log)
+  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'.','log','error.log'),"a+")
+  error_logger.sync = true
+
+  configure do
+    use ::Rack::CommonLogger, access_logger
+  end
+
+  before {
+    env["rack.errors"] =  error_logger
+  }
 
   post '/payload' do
     status 204
