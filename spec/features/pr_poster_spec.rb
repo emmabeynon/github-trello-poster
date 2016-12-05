c304799992e6358d01cdb297f246d60b82e0d21b
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-12-05 14:18:07 +0000

    Improve readability of GithubPullRequest
    I've made two changes here
    
    One was to allow the next person to understand what are those values
    passed to GithubPullRequest.
    The second was to make its tests a bit more usable and less repetitive

--- spec/features/pr_poster_spec.rb
+++ spec/features/pr_poster_spec.rb
@@ -3,7 +3,25 @@ require 'github_pull_request'
 describe 'Pull Request Poster' do
   WebMock.allow_net_connect!
   let(:trello_poster) { TrelloPoster.new(ENV['TRELLO_PUBLIC_KEY'], ENV['TRELLO_MEMBER_TOKEN']) }
-  subject(:github_pr) { GitHubPullRequest.new(60356369, 1, false, trello_poster) }
+
+  subject(:github_pr) do
+    GitHubPullRequest.new(
+      repo: 60356369,
+      pull_request_id: 1,
+      merged: false,
+      trello_poster: trello_poster
+    )
+  end
+
+  let(:github_pull_request_params) do
+    {
+      repo: 60356369,
+      pull_request_id: 1,
+      merged: false,
+      trello_poster: trello_poster
+    }
+  end
+
   let(:pull_request) { github_pr.login_user.pull_request(60356369, 1) }
   let(:trello_card_id) { pull_request.body.match(/https:\/\/trello.com\/c\/\w{8}/)[0].gsub(/https:\/\/trello.com\/c\//, '') }
   let(:trello_card) { github_pr.trello_poster.client.find(:card, trello_card_id) }
@@ -24,21 +42,21 @@ describe 'Pull Request Poster' do
 
   context "when a pull request URL is already in a card's checklist" do
     it 'does not post a pull request URL to the Trello card' do
-      GitHubPullRequest.new(60356369, 1, false, trello_poster)
+      GitHubPullRequest.new(github_pull_request_params)
       expect(@checklist.check_items.count).to eq(1)
     end
   end
 
   context "when a pull request has been merged" do
     it "checks the Pull Request off the checklist" do
-      GitHubPullRequest.new(60356369, 1, true, trello_poster)
+      GitHubPullRequest.new(github_pull_request_params.merge(merged: true))
       updated_checklist = trello_card.checklists.first
       expect(updated_checklist.check_items.first['state']).to eq('complete')
     end
   end
 
   it 'checks a Pull Request checkbox if the PR has been updated with a Trello URL after it was merged' do
-    github_pull_request = GitHubPullRequest.new(60356290, 3, true, trello_poster)
+    github_pull_request = GitHubPullRequest.new(github_pull_request_params.merge(repo: 60356290))
     merged_pull_request = github_pull_request.login_user.pull_request(60356290, 1)
     trello_card = github_pull_request.trello_poster.client.find(:card, 'Bjdv5qRr')
     checklist = trello_card.checklists.first
