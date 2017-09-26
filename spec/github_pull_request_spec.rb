f7cdad3cbd034e8dc843fc7b58182f2effe738c2
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-09-26 15:25:17 +0100

    Refactor GitHubPullRequest class
    * We introduce keyword arguments upon initialize so that it's clearer what data is being passed in
    * We remove `fetch_pull_request_data` and introduce `call` so it's less opaque what steps occur in the class
    * We make some attributes private as necessary
    * Tests have been updated accordingly

--- spec/github_pull_request_spec.rb
+++ spec/github_pull_request_spec.rb
@@ -3,14 +3,28 @@ require 'ostruct'
 
 describe GitHubPullRequest do
   let(:trello_poster) { double(TrelloPoster) }
+
   let(:repo_pull_request) do
     OpenStruct.new({
       html_url: 'https://github.com/gov-test-org/project-b/pull/1',
       body: 'A commit with a Trello URL https://trello.com/c/6wQLN2C7/21-add-randomfile-1-txt-to-project-b'
     })
   end
-  let(:octokit) { double Octokit::Client, login: 'username', pull_request: repo_pull_request }
-  subject(:github_pr) { GitHubPullRequest.new(60356369, 1, false, trello_poster) }
+
+  let(:octokit) do
+    double Octokit::Client,
+      login: 'username',
+      pull_request: repo_pull_request
+  end
+
+  let(:github_pull_request_params) do
+    {
+      repo_id: 60356369,
+      pull_request_id: 1,
+      merged: false,
+      trello_poster: trello_poster
+    }
+  end
 
   before(:each) do
     allow(Octokit::Client).to receive(:new).and_return(octokit)
@@ -18,19 +32,34 @@ describe GitHubPullRequest do
   end
 
   describe 'Default' do
-    it 'initializes with login_user set to an authenticated Github user' do
-      expect(github_pr.login_user).to have_attributes(login: 'username')
+    let(:github_pr) { GitHubPullRequest.new(github_pull_request_params) }
+    it 'initializes with a pull_request_id' do
+      expect(github_pr.pull_request_id).to eq(1)
     end
 
-    it 'initializes with a merge status set' do
-      expect(github_pr.merged).to be false
+    it 'initializes with a repo_id' do
+      expect(github_pr.repo_id).to eq(60356369)
     end
   end
 
-  describe '#fetch_pull_request_data' do
-    it 'retrieves HTML URL and body data for the given URL and passes to the check_for_trello_card method, leading to GitHubPullRequest being instantiated' do
-      expect(trello_poster).to receive(:post!).with(repo_pull_request[:html_url], '6wQLN2C7', false)
-      GitHubPullRequest.new(60356369, 1, false, trello_poster)
+  describe '#call' do
+    context "when a pull request body contains a valid Trello URL" do
+      it "instantiates GitHubPullRequest" do
+        expect(trello_poster).to receive(:post!)
+          .with(repo_pull_request[:html_url], '6wQLN2C7', false)
+        GitHubPullRequest.new(github_pull_request_params).call
+      end
+    end
+
+    context "when a pull request body does not contain a valid Trello URL" do
+      before do
+        repo_pull_request.body = "A commit without a Trello URL"
+      end
+
+      it "does not instantiate GitHubPullRequest" do
+        expect(trello_poster).not_to receive(:post!)
+        GitHubPullRequest.new(github_pull_request_params).call
+      end
     end
   end
 end
